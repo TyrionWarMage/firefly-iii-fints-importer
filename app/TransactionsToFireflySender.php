eab0896de27447b42551822159a4a9432f4bc1d0
Tyrion <Tyrion@host.docker.internal>
2024-12-09 12:44:02 +0100

    WIP

--- app/TransactionsToFireflySender.php
+++ app/TransactionsToFireflySender.php
@@ -52,6 +52,7 @@ class TransactionsToFireflySender
         string $regex_match, string $regex_replace
     )
     {
+        echo $transaction;
         $debitOrCredit = $transaction->getCreditDebit();
         $amount        = $transaction->getAmount();
         $source        = array('id' => $firefly_account_id);
